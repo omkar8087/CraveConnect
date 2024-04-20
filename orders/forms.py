ede324bb95005d7c9e65387a37a72440057f421b
Omkar <jadhavomkar8087@gmail.com>
2024-04-20 16:01:26 +0530

    nothing

--- orders/forms.py
+++ orders/forms.py
@@ -7,3 +7,7 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ['first_name', 'last_name', 'phone', 'email', 'address', 'country', 'state', 'city', 'pin_code']
         
+# class OrderForm(forms.ModelForm):
+#     class Meta:
+#         model = Order
+#         fields = ['first_name', 'last_name', 'phone', 'email', 'address', 'country', 'state', 'city', 'pin_code']
