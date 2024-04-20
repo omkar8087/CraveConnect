0fa39bc62ae17ac9109164513223b11b022d2009
Omkar <jadhavomkar8087@gmail.com>
2024-04-20 15:44:15 +0530

    deployment

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.105.36.144', '127.0.0.1', 'craveconnect.shop', 'www.craveconnect.shop']
 
 
 # Application definition
